1b49c3c33b930c68dcb6769c0a43faf2e9eab777
Artur Dryomov <me@ming13.ru>
2011-08-22 23:04:04 +0300

    Added auto updating with special intervals

--- meg.py
+++ meg.py
@@ -5,6 +5,10 @@ import gobject
 from time import time
 
 
+MINUTE_LENGTH = 6
+UPDATE_INTERVAL = 1
+
+
 class Controller:
     def __init__(self):
         self.state = "working"
@@ -12,30 +16,32 @@ class Controller:
         self.timer = Timer()
 
     def update(self):
-        if state == "working":
-            if time() > timer.get_long_rest_time():
-                gui.call_long_rest_window()
-                timer.update_long_rest_time()
-            elif time() > timer.get_short_rest_time():
-                gui.call_short_rest_window()
-                timer.update_short_rest_time()
-        gobject.timeout_add_seconds(30, self.update)
+        if self.state == "working":
+            if time() > self.timer.get_long_rest_time():
+                self.gui.call_rest_window(time() + self.timer.long_rest_length)
+                self.timer.update_long_rest_time()
+            elif time() > self.timer.get_short_rest_time():
+                self.gui.call_rest_window(time() + self.timer.short_rest_length)
+                self.timer.update_short_rest_time()
+        gobject.timeout_add_seconds(UPDATE_INTERVAL, self.update)
 
     def main(self):
+        gobject.timeout_add_seconds(UPDATE_INTERVAL, self.update)
         self.gui.main()
-        gobject.timeout_add_seconds(30, self.update)
 
 
 class Timer:
     def __init__(self):
-        self.short_rest_time = time() + 60 * 15
-        self.long_rest_time = time() + 60 * 60
+        self.short_rest_length = MINUTE_LENGTH * 1
+        self.long_rest_length = MINUTE_LENGTH * 5
+        self.short_rest_time = time() + MINUTE_LENGTH * 15
+        self.long_rest_time = time() + MINUTE_LENGTH * 60
     
     def update_short_rest_time(self):
-        self.short_rest_time += 60 * 15
+        self.short_rest_time += MINUTE_LENGTH * 15
 
     def update_long_rest_time(self):
-        self.long_rest_time += 60 * 60
+        self.long_rest_time += MINUTE_LEGNTH * 60
 
     def get_short_rest_time(self):
         return self.short_rest_time
@@ -57,15 +63,24 @@ class GUI:
         close_menu_item = gtk.ImageMenuItem(gtk.STOCK_QUIT)
         close_menu_item.connect_object("activate", lambda w: gtk.main_quit(), "Close")
         tray_icon_menu.append(close_menu_item)
-        #close_menu_item.show()
         map(lambda i: i.show(), tray_icon_menu)
         tray_icon_menu.popup(None, None, None, event_button, event_time)
 
-    def call_short_rest_window(self):
-        pass
+    def call_rest_window(self, rest_time):
+        rest_window = gtk.Window()
+        rest_window.set_position(gtk.WIN_POS_CENTER)
+        rest_window.set_border_width(10)
+        label = gtk.Label("Meg")
+        rest_window.add(label)
+        rest_window.show_all()
 
-    def call_long_rest_window(self):
-        pass
+        def update_window_state():
+            if time() > rest_time:
+                rest_window.destroy()
+                return
+            else:
+                gobject.timeout_add_seconds(UPDATE_INTERVAL, update_window_state)
+        gobject.timeout_add_seconds(UPDATE_INTERVAL, update_window_state)
 
     def main(self):
         gtk.main()
